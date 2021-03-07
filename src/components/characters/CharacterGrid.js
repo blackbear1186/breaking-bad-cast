c181f8bc388ea0fbe9e7f160718ed596152dbf8c
Berlin Johnson <berlin.johnson@protonmail.com>
2021-03-07 13:12:08 +0900

    Created front and back of card

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
+import CharacterItem from './CharacterItem';
 
 const CharacterGrid = ({ items, isLoading }) => {
   // check if loading display spinner, else map through items and output as cards
   // use parenthesis because we are rendering jsx
-  // add key to h1 to remove key warning
+  // 1. add key to h1 to remove key warning
+  // 2. Change h1 to CharacterItem and pass item into component
   return isLoading ? <h1>Loading...</h1> : <section className="cards">
     {items.map(item => (
-      <h1 key={item.char_id}>{item.name}</h1>
+      <CharacterItem key={item.char_id} item={item}></CharacterItem>
     ))}
   </section>
 }
